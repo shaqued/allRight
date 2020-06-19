d862e1e59a3cf27362e4e2c791fb21938b0d4a64
Noafridman <noaf256@gmail.com>
2020-06-19 11:05:49 +0300

    add seed prices

--- server/api/price/price.seed.js
+++ server/api/price/price.seed.js
@@ -53,6 +53,106 @@ export default {
                     price: 200
                 }
             ]
+        },
+        {
+            businessRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 100,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.onlyMe,
+                    price: 150
+                },
+                {
+                    rangeMin: 100,
+                    rangeMax: 300,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.single,
+                    price: 300
+                }
+            ],
+            privateRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 50,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.onlyMe,
+                    price: 100
+                },
+                {
+                    rangeMin: 50,
+                    rangeMax: 150,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.single,
+                    price: 150
+                }
+            ],
+            socialRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 70,
+                    mediaType: MediaType.online,
+                    usageType: UsageType.onlyMe,
+                    price: 100
+                },
+                {
+                    rangeMin: 70,
+                    rangeMax: 170,
+                    mediaType: MediaType.online,
+                    usageType: UsageType.single,
+                    price: 200
+                }
+            ]
+        },
+        {
+            businessRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 130,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.onlyMe,
+                    price: 300
+                },
+                {
+                    rangeMin: 130,
+                    rangeMax: 300,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.single,
+                    price: 400
+                }
+            ],
+            privateRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 100,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.onlyMe,
+                    price: 100
+                },
+                {
+                    rangeMin: 100,
+                    rangeMax: 150,
+                    mediaType: MediaType.offline,
+                    usageType: UsageType.single,
+                    price: 190
+                }
+            ],
+            socialRangePrice: [
+                {
+                    rangeMin: 1,
+                    rangeMax: 40,
+                    mediaType: MediaType.online,
+                    usageType: UsageType.onlyMe,
+                    price: 100
+                },
+                {
+                    rangeMin: 40,
+                    rangeMax: 120,
+                    mediaType: MediaType.online,
+                    usageType: UsageType.single,
+                    price: 200
+                }
+            ]
         }
 ]
 };
