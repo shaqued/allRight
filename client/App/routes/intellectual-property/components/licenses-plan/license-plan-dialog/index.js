d43d19aaa4f0f39a0b596f66e35405ede29af97b
shaqued <shaqued201@gmail.com>
2020-06-26 11:41:42 +0300

    add row selection and button disable when none selected

--- client/App/routes/intellectual-property/components/licenses-plan/license-plan-dialog/index.js
+++ client/App/routes/intellectual-property/components/licenses-plan/license-plan-dialog/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import useStyles from './license-plan-dialog.css';
-import { Dialog, DialogTitle, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
+import { Dialog, DialogTitle, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Checkbox } from '@material-ui/core';
 
 export default ({ onClose, open, priceRange }) => {
     const classes = useStyles();
+    const [selected, setSelected] = React.useState();
+
+    const isSelected = row => selected === row;
+
+    const handleClick = (event, row) => {
+        if (selected === row) {
+            setSelected();
+        } else {
+            setSelected(row);
+        }
+      };
 
     const handleClose = () => {
         onClose();
@@ -23,17 +34,32 @@ export default ({ onClose, open, priceRange }) => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {priceRange.map((row) => (
-                            <TableRow key={row.min}>
-                                <TableCell align="right">{row.rangeMin} - {row.rangeMax}</TableCell>
-                                <TableCell align="right">{row.usageType}</TableCell>
-                                <TableCell align="right">{row.price} ש"ח</TableCell>
-                            </TableRow>
-                        ))}
+                        {priceRange.map((row) => {
+                            const isItemSelected = isSelected(row);
+
+                            return (
+                                <TableRow key={row.min}
+                                    hover
+                                    onClick={(event) => handleClick(event, row)}
+                                    role="checkbox"
+                                    aria-checked={isItemSelected}
+                                    tabIndex={-1}
+                                    key={row.name}
+                                    selected={isItemSelected}>
+                                    <TableCell padding="checkbox">
+                                        <Checkbox
+                                            checked={isItemSelected}
+                                        /></TableCell>
+                                    <TableCell align="right">{row.rangeMin} - {row.rangeMax}</TableCell>
+                                    <TableCell align="right">{row.usageType}</TableCell>
+                                    <TableCell align="right">{row.price} ש"ח</TableCell>
+                                </TableRow>
+                            )
+                        })}
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Button color="primary">למעבר לחוזה</Button>
+            <Button  disabled={!selected} color="primary">למעבר לחוזה</Button>
         </Dialog>
     );
 }
