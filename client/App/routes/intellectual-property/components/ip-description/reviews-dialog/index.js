81e43c2fa6091e3ba17e199241fe854fa29ebe63
Noafridman <noaf256@gmail.com>
2020-08-04 17:11:00 +0300

    add review for shaqued

--- client/App/routes/intellectual-property/components/ip-description/reviews-dialog/index.js
+++ client/App/routes/intellectual-property/components/ip-description/reviews-dialog/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useStyles from "./reviews-dialog.css";
+import {Rating} from '@material-ui/lab';
 import {
   Dialog,
   Stepper,
@@ -12,11 +13,17 @@ import {
   CardContent,
   Avatar,
   CardActions,
+  TextField
 } from "@material-ui/core";
 import {formatDate} from '../../../../../../common/Util';
 
 export default ({ onClose, open, ip }) => {
   const classes = useStyles();
+  const [writeReview, setwriteReview] = React.useState(false);
+
+  const addReview = () => {
+    setwriteReview(true);
+  };
 
   const getUserInitials = (userName) => {
     console.log(ip.reviews);
@@ -32,6 +39,22 @@ export default ({ onClose, open, ip }) => {
   return (
     <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
       <div className={classes.dialog}>
+        { !writeReview ? null :  <Card className={classes.root}>
+          <CardHeader
+            avatar={
+              <Avatar className={classes.avatar}>
+
+              </Avatar>
+            }
+            title="username"
+            subheader={formatDate(new Date())}
+          />
+          <CardContent className={classes.cardContent}>
+            <TextField labelWidth={60} id="review" label="ביקורת" variant="outlined" />
+            <Rating value={5} />
+          </CardContent>
+        </Card>
+        }
         {ip.reviews &&
           ip.reviews.map((review) => (
             <Card className={classes.root}>
@@ -44,14 +67,17 @@ export default ({ onClose, open, ip }) => {
                 title={review.userName}
                 subheader={formatDate(new Date(review.creationDate))}
               />
-              <CardContent>
+              <CardContent className={classes.cardContent}>
                 <Typography variant="body2" color="textPrimary" component="p">
                   {review.comment}
                 </Typography>
+                <Rating value={review.scoring} readOnly />
               </CardContent>
-              <CardActions disableSpacing></CardActions>
             </Card>
           ))}
+        {writeReview ? null : <Button variant='outlined' color="primary" onClick={() => addReview()}>
+          הוסף ביקורת
+        </Button>}
       </div>
     </Dialog>
   );
--- client/App/routes/intellectual-property/components/ip-description/reviews-dialog/reviews-dialog.css.js
+++ client/App/routes/intellectual-property/components/ip-description/reviews-dialog/reviews-dialog.css.js
@@ -1,11 +1,23 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { red } from '@material-ui/core/colors';
+import {makeStyles} from "@material-ui/core/styles";
+import {red} from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
+  root: {
+    display: 'inline-flex',
+    flexDirection: 'row',
+    minWidth: '96%',
+    margin: '2%'
+  },
   dialog: {
-    margin: "10px",
+    margin: "2%",
   },
   avatar: {
     backgroundColor: red[500],
+    marginLeft: "10px"
   },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%'
+  }
 }));
