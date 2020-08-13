ffa933645a2e5186de2519d194bf8c577850ef31
idoperach <idoperach@gmail.com>
2020-08-14 00:28:32 +0300

    fixed server, wip
    should look for more function that caused problems before uploading this (should be in one commit)
    
    also didn't check client with dagan's and amit's branches
    
    it took a while to understand the problem..

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -1,14 +1,12 @@
 import jwt from 'jsonwebtoken';
 import expressJwt from 'express-jwt';
 import pify from 'pify';
-import createError from 'http-errors';
-import User from '../api/user/user.model';
 
 const validateJwt = pify(expressJwt({secret: process.env.SESSION_SECRET}));
 
 export const isAuthenticated = () => async (req, res) => {
   // Allow access_token to be passed through cookies parameter as well
-  if (req.cookies && req.cookies.hasOwnProperty('userToken')) {
+  if (req.cookies && req.cookies.userToken) {
     req.headers.authorization = `Bearer ${req.cookies.userToken}`;
   }
 
